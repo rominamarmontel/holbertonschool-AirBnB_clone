8ac17bdf5ca46f0ee6f615912c13e93681418d4e
LucileSD <4316@holbertonschool.com>
2022-06-27 12:33:23 +0000

    first success

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@
 """
 from datetime import datetime
 import uuid
-import datetime
+from datetime import date
 
 
 """
@@ -16,7 +16,7 @@ class BaseModel:
     """
         base class of the console
     """
-    def __init__(self, id, created_at, updated_at):
+    def __init__(self):
         """
             initiate the id of an instance with the data time
             args:
@@ -25,34 +25,31 @@ class BaseModel:
                 updated_at: the current datetime when an instance is created
                     and it will be updated when an object is changed
         """
-        self.id = str(uuid.uuid4(id))
-        self.created_at = datetime.datetime(created_at)
-        self.updated_at = updated_at
+        self.id = str(uuid.uuid4())
+        self.created_at = str(datetime.now())
+        self.updated_at = str(datetime.now())
 
-    @classmethod
-    def __str__(cls, self):
+    def __str__(self):
         """
             return a string with the information of the object
         """
-        return f"[{cls.__name__}] ({self.id}) {self.__dict__}"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
-    def __save__(self):
+    def save(self):
         """
             updates the public instance attribute
             updated_at with the current datetime
         """
-        return datetime.datetime(self.updated_at)
+        self.updated_at = datetime.now()
 
-    def to_dict(cls, self):
+    def to_dict(self):
         """
             returns a dictionary containing all keys/values of
             __dict__ of the instance 
         """
-        dict = {"my_number": cls.__name__,
-                "name": cls.__name__,
-                "created_at": self.created_at,
-                "updated_at": self.updated.at,
-                "id": self.id}
+        dict = {"updated_at": self.updated_at,
+                "id": self.id,
+                "created_at": self.created_at,}
         return dict
 
 
