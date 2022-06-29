6395d33c52548c2edb0d0b8aa10d3938215d9190
LucileSD <4316@holbertonschool.com>
2022-06-29 15:21:25 +0000

    console

--- console.py
+++ console.py
@@ -4,8 +4,11 @@
 """
 import cmd
 import sys
+from sys import argv
 from models.base_model import BaseModel
 import models
+from models.engine.file_storage import FileStorage
+import json
 
 
 """
@@ -27,6 +30,10 @@ class HBNBCommand(cmd.Cmd):
         """EOF command to exit the program """
         sys.exit()
 
+    def emptyline(self):
+        """ empty line command """
+        pass
+
     def do_create(self, arg):
         """create command creates a new instance of BaseModell"""
         if not arg:
@@ -34,9 +41,68 @@ class HBNBCommand(cmd.Cmd):
         elif arg != "BaseModel":
             print("** class doesn't exist **")
         else:
-            BaseModel()
-            models.save()
-            print(f"{self.id}")
+            self = BaseModel()
+            BaseModel.save(self)
+            print(self.id)
+
+    def do_show(self, arg):
+        """
+            show command prints the string representation of an instance
+            based on the class name and id
+        """
+        array = arg.split()
+        if len(array) < 1:
+            print("** class name missing **")
+        elif array[0] != "BaseModel":
+            print("** class doesn't exist **")
+        elif len(array) < 2:
+            print("** instance id missing **")
+        else:
+            with open('file.json', 'r', encoding="utf-8") as fd:
+                info = json.load(fd)
+                for key in info:
+                    ar = key.split(".")
+                    if array[1] == ar[1]:
+                        print(info.get("BaseModel.{}".format(ar[1])))
+                if array[1] != ar[1]:
+                    print("** no instance found **")
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and id"""
+        array = arg.split()
+        if len(array) < 1:
+            print("** class name missing **")
+        elif array[0] != "BaseModel":
+            print("** class doesn't exist **")
+        elif len(array) < 2:
+            print("** instance id missing **")
+        else:
+            with open("file.json", "r", encoding="utf-8") as flj:
+                list = json.load(flj)
+                for key in list:
+                    ar = key.split(".")
+                    if array[1] == ar[1]:
+                        new_list = "BaseModel.{}".format(ar[1])
+
+                if array[1] != ar[1]:
+                        print("** no instance found **")
+                        return
+
+            list.pop(new_list)
+            with open("file.json", 'w', encoding="utf-8") as f:
+                json.dump(list, f)
+                
+
+    """def do_all(self, arg):
+        Prints all string representation of all instances
+        array = arg.split()
+        if array[0] != "BaseModel":
+            print("** class doesn't exist **")
+        else:"""
+            
+
+
+
 
 
 
