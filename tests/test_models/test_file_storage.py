efe02c9b7714c1e463b9737e3a1ecaa0fba07207
Romi <romi@MacBook-Pro-de-Romi.local>
2022-07-01 17:17:37 +0200

    test_file_storage up

--- tests/test_models/test_file_storage.py
+++ tests/test_models/test_file_storage.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """ Unit test for FileStorage """
 
+from multiprocessing import dummy
 import unittest
 import pycodestyle
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from datetime import datetime
+import os
+import json
 
 
 class TestFileStorage(unittest.TestCase):
@@ -21,27 +24,32 @@ class TestFileStorage(unittest.TestCase):
         """Test method all"""
         storage = FileStorage()
         test1 = storage.all()
+        self.assertIsNotNone(test1)
         self.assertEqual(type(test1), dict)
 
-    def test_new(self):
-        """Test method new"""
-        storage = FileStorage()
-        test2 = storage.new()
-        model = BaseModel()
-        storage.new(model)
-
-
-
-
-
-    def test_save(self):
-        """Test method save"""
+    def test_save1(self):
+        """Test method save if the type of file is dictionary"""
         storage = FileStorage()
         storage.save()
+        with open("file.json", "r", encoding="utf-8") as f:
+            file = json.load(f)
+        self.assertTrue(type(file), dict)
 
-    def reload(self):
+    def test_save2(self):
+        """Test method save to verify json file exists"""
+        storage = FileStorage()
+        storage.save()
+        self.assertTrue(os.path.exists("file.json"))
+
+    def reload1(self):
         """Test method reload"""
+        self.assertEqual(storage.reload(), None)
 
+    def reload2(self):
+        """Test method reload"""
+        jsontype = '{"__class__": "BaseModel"}'
+        list = json.loads(jsontype)
+        self.assertTrue(type(list) is dict)
 
 if __name__ == '__main__':
     unittest.main()
