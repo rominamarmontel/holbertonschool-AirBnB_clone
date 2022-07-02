061a6c756d7f1ce8c77e6fe1664bce8f24b691fb
Romi <romi@MacBook-Pro-de-Romi.local>
2022-07-02 15:50:10 +0200

    test up

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Unittest module for State"""
+from re import S
 from models.base_model import BaseModel
 from models.state import State
 import unittest
@@ -10,19 +11,19 @@ class TestState(unittest.TestCase):
 
     def test_init(self):
         """test the creation of an instance"""
-        a = State()
-        self.assertEqual(a.name, '')
-        a.name = 'Houssem'
-        self.assertEqual(a.name, 'Houssem')
-        self.assertTrue(hasattr(a, 'id'))
-        self.assertTrue(hasattr(a, 'created_at'))
-        self.assertTrue(hasattr(a, 'updated_at'))
-        self.assertTrue(issubclass(a.__class__, BaseModel))
+        s = State()
+        self.assertEqual(s.name, '')
+        s.name = 'Houssem'
+        self.assertEqual(s.name, 'Houssem')
+        self.assertTrue(hasattr(s, 'id'))
+        self.assertTrue(hasattr(s, 'created_at'))
+        self.assertTrue(hasattr(s, 'updated_at'))
+        self.assertTrue(issubclass(s.__class__, BaseModel))
 
     def test_str(self):
         """Test the string output of the class"""
-        a = State()
-        self.assertEqual(str(a), f"[State] ({a.id}) {a.__dict__}")
+        s = State()
+        self.assertEqual(str(s), f"[State] ({s.id}) {s.__dict__}")
 
 
 if __name__ == '__main__':
