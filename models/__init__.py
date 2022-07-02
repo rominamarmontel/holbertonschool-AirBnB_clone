b91f27038a7fd869f8fdb90a2c13041001e91f4c
Romi <romi@MacBook-Pro-de-Romi.local>
2022-07-02 09:52:33 +0200

    modification import

--- models/__init__.py
+++ models/__init__.py
@@ -3,13 +3,6 @@
     import file_storage
 """
 from models.engine.file_storage import FileStorage
-from models.user import User
-from models.state import State
-from models.review import Review
-from models.place import Place
-from models.city import City
-from models.amenity import Amenity
-from models.base_model import BaseModel
 
 
 """
